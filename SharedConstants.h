7ba49bb48d4413f9022354eab12b570febc3c2f2
pktmterenceg <terencegoggin@gmail.com>
2011-11-16 21:03:57 -0500

    Added grid overlay, direct printing/saving to PDF

---
+++ SharedConstants.h
@@ -0,0 +1,12 @@
+/*
+ *  SharedConstants.h
+ *  FormulatePro
+ *
+ *  Created by Terence Goggin on 10/26/11.
+ *  Copyright 2011 IAA. All rights reserved.
+ *
+ */
+
+#define kFPShowTipsAtStartup	@"FPShowTipsAtStartup"
+#define kFPNextToolTip			@"FPNextToolTip"
+#define kFPShowGridOverlay		@"FPShowGridOverlay"
